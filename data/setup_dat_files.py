9b3fa2b9f6bcbc441f21991501193125c65fe47f
Joep Boumans <joep_boumans@live.nl>
2024-07-10 18:00:08 +0200

    Combine pcap parsing and .dat creation

--- data/setup_dat_files.py
+++ data/setup_dat_files.py
@@ -1,14 +1,21 @@
+import glob
+import subprocess
 
-with open('test.txt', 'r') as fin, open('test.out', 'wb') as fout:
-    lines = fin.readlines()
-    for l in lines:
-        print(l)
-        l = l.split()
-        if len(l) < 4:
-            continue
-        out = [int(num).to_bytes(1, byteorder='big') for e in l[:2] for num in e.split('.')]
-        out += [int(num).to_bytes(2, byteorder='big') for num in l[2:4]]
-        out += [int(num).to_bytes(1, byteorder='big') for num in l[4]]
-        print(out)
-        for o in out:
-            fout.write(o)
+# Transform .pcap files into text fileso
+for file in glob.glob("*.pcap"):
+    print("Parsing %s into 5-tuple..."% file)
+    out =subprocess.run(["tshark", "-r", file, "-T", "fields", "-e", "ip.src","-e", "ip.dst","-e", "tcp.srcport","-e", "tcp.dstport","-e", "ip.proto"], capture_output=True, text=True)
+    with open(file.replace("pcap","out"), 'wb') as fout:
+        lines = out.stdout.split('\n')
+        for l in lines:
+            # print(l)
+            l = l.split()
+            if len(l) < 4:
+                continue
+            out = [int(num).to_bytes(1, byteorder='big') for e in l[:2] for num in e.split('.')]
+            out += [int(num).to_bytes(2, byteorder='big') for num in l[2:4]]
+            out += [int(num).to_bytes(1, byteorder='big') for num in l[4]]
+            # print(out)
+            for o in out:
+                fout.write(o)
+    print("Finished parsing %s"% file)

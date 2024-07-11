988bc126730523df6c58c275135e477167d7b318
Joep Boumans <joep_boumans@live.nl>
2024-07-11 14:42:38 +0200

    Create counter

---
+++ src/counter.h
@@ -0,0 +1,38 @@
+#ifndef _COUNTER_H
+#define _COUNTER_H
+
+#include <cstdint>
+template <typename T> class Counter {
+public:
+  T count;
+  uint32_t max_count;
+  bool overflow = false;
+  Counter(uint32_t max) {
+    this->count = 0;
+    this->max_count = max;
+  }
+
+  uint32_t increment(uint32_t c = 1) {
+    if (this->overflow) {
+      return 1;
+    }
+    this->count += c;
+    if (this->count >= this->max_count) {
+      this->overflow = true;
+    }
+    return 0;
+  }
+
+  uint32_t decrement(uint32_t c = 1) {
+    if (this->overflow) {
+      this->overflow = false;
+    }
+    if (this->count > 0) {
+      this->count -= c;
+      return 0;
+    }
+    return 1;
+  }
+};
+
+#endif // !_COUNTER_H

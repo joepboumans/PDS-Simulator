b2fd31f15d8e29bf35fc916c56e834ba03b77aee
Joep Boumans <joep_boumans@live.nl>
2024-07-11 13:35:47 +0200

    Set filename as path_to_file

--- src/data-parser.h
+++ src/data-parser.h
@@ -1,24 +1,21 @@
 #ifndef _DATA_PARSER_H_
 #define _DATA_PARSER_H_
 
-
-#include <bitset>
-#include <cstdio>
-#include <iostream>
-#include <fstream>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <cassert>
 #include "common.h"
+#include <bitset>
+#include <cassert>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <sys/stat.h>
+#include <unistd.h>
 
 class dataParser {
 public:
-
-  uint32_t get_traces(char* filename, TRACE &trace) {
+  uint32_t get_traces(char *filename, TRACE &trace) {
     // Load filename
     sprintf(this->filename, "%s", filename);
-    sprintf(this->path_to_file, "data/%s", filename);
-    
+
     // Verify if the trace is present or needs to be downloaded
     if (!this->check_for_traces()) {
       int result = this->download_data_traces();
@@ -28,7 +25,7 @@ public:
     }
 
     std::cout << "Found file, opening..." << std::endl;
-    this->fin = fopen(this->path_to_file, "rb");
+    this->fin = fopen(this->filename, "rb");
     assert(this->fin);
 
     std::cout << "Opened, finding size..." << std::endl;
@@ -38,13 +35,13 @@ public:
     fseek(this->fin, 0L, SEEK_SET);
     std::cout << "Size of trace " << this->filename << ": " << sz << std::endl;
 
-    // Create 5-tuple and trace 
+    // Create 5-tuple and trace
     FIVE_TUPLE tin;
     // Load trace with 5-tuple
     if (this->fin == NULL) {
       std::cout << "ERROR: Input file is NULL" << std::endl;
     }
-    while(fread(&tin, 1, sizeof(FIVE_TUPLE), this->fin)) {
+    while (fread(&tin, 1, sizeof(FIVE_TUPLE), this->fin)) {
       trace.push_back(tin);
     }
 
@@ -58,13 +55,19 @@ private:
 
   uint32_t download_data_traces() {
     char command[200];
-    sprintf(command, "%s %s%s", "wget https://www.dropbox.com/s/bajuqha884cxgwl/data1.dat -P", get_current_dir_name(), "/data/");
-    std::cout << "\033[1;31m A sample data (at data/) does not exist, so start to download (~260 MB)...\033[0m" << std::endl;
+    sprintf(command, "%s %s%s",
+            "wget https://www.dropbox.com/s/bajuqha884cxgwl/data1.dat -P",
+            get_current_dir_name(), "/data/");
+    std::cout << "\033[1;31m A sample data (at data/) does not exist, so start "
+                 "to download (~260 MB)...\033[0m"
+              << std::endl;
 
     int wget_result = std::system(command);
     std::cout << "Download result " << wget_result << std::endl;
     if (!this->check_for_traces()) {
-      std::cout << "\033[1;31m Download is failed. Please see the src/data-parser.h\033[0m" << std::endl;
+      std::cout << "\033[1;31m Download is failed. Please see the "
+                   "src/data-parser.h\033[0m"
+                << std::endl;
     }
 
     return wget_result;
@@ -72,7 +75,7 @@ private:
 
   uint32_t check_for_traces() {
     struct stat buffer;
-    return (stat (this->path_to_file, &buffer) == 0);
+    return (stat(this->filename, &buffer) == 0);
   }
 };
 #endif

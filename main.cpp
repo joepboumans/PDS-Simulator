240dac4ef080c5211cbec7a3c72dabf55dbffaeb
Joep Boumans <joep_boumans@live.nl>
2024-07-11 13:33:58 +0200

    Parse in .dat file and load them

--- main.cpp
+++ main.cpp
@@ -1,17 +1,36 @@
-#include <iostream>
 #include "src/common.h"
 #include "src/data-parser.h"
+#include <cstddef>
+#include <cstring>
+#include <glob.h>
+#include <iostream>
+#include <iterator>
 
-int main(){
+int main() {
   dataParser data_parser;
-  // char filename[100] = "data1.dat";
-  char filename[100] = "test.out";
-  TRACE trace;
-  data_parser.get_traces(filename, trace);
+  // Get data files
+  glob_t glob_res;
+  memset(&glob_res, 0, sizeof(glob_res));
+  glob("data/*.dat", GLOB_TILDE, NULL, &glob_res);
+  vector<std::string> filenames;
+  for (size_t i = 0; i < glob_res.gl_pathc; ++i) {
+    filenames.push_back(std::string(glob_res.gl_pathv[i]));
+  }
 
-  std::cout << "Trace data:" << std::endl;
-  for (int i = 0; i < 10; i++) {
-    print_five_tuple(trace.at(i));
+  // Load in traces
+  TRACE traces[std::size(filenames)];
+  std::size_t i = 0;
+  for (auto f : filenames) {
+    std::cout << f << std::endl;
+    data_parser.get_traces(f.data(), traces[i++]);
+  }
+
+  // Loop over traces
+  for (auto trace : traces) {
+    std::cout << "Trace data:" << std::endl;
+    for (int i = 0; i < 10; i++) {
+      print_five_tuple(trace.at(i));
+    }
   }
   return 0;
 }

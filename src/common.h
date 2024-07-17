771ddccdca15e71f44d42c383eb1bc23e11615a6
Joep Boumans <joep_boumans@live.nl>
2024-07-17 12:51:30 +0200

    Add inserting and printing

--- src/common.h
+++ src/common.h
@@ -2,6 +2,7 @@
 #define _COMMON_H
 
 #include <cstdio>
+#include <cstring>
 #include <iostream>
 #include <vector>
 
@@ -15,17 +16,20 @@ struct FIVE_TUPLE {
 
 inline void print_five_tuple(const FIVE_TUPLE tuple) {
   char srcIp[16];
-  sprintf(srcIp, "%i.%i.%i.%i", (int) tuple.srcIp[0], (int) tuple.srcIp[1], (int) tuple.srcIp[2], (int) tuple.srcIp[3]);
+  sprintf(srcIp, "%i.%i.%i.%i", (int)tuple.srcIp[0], (int)tuple.srcIp[1],
+          (int)tuple.srcIp[2], (int)tuple.srcIp[3]);
   char destIp[16];
-  sprintf(destIp, "%i.%i.%i.%i", (int) tuple.srcIp[0], (int) tuple.srcIp[1], (int) tuple.srcIp[2], (int) tuple.srcIp[3]);
+  sprintf(destIp, "%i.%i.%i.%i", (int)tuple.srcIp[0], (int)tuple.srcIp[1],
+          (int)tuple.srcIp[2], (int)tuple.srcIp[3]);
   char srcPort[6];
-  sprintf(srcPort, "%i", (int) ((tuple.srcPort[0] << 8) | tuple.srcPort[1]));
+  sprintf(srcPort, "%i", (int)((tuple.srcPort[0] << 8) | tuple.srcPort[1]));
   char dstPort[6];
-  sprintf(dstPort, "%i", (int) ((tuple.dstPort[0] << 8) | tuple.dstPort[1]));
+  sprintf(dstPort, "%i", (int)((tuple.dstPort[0] << 8) | tuple.dstPort[1]));
   char protocol[6];
-  sprintf(protocol, "%i", (int) tuple.protocol);
-  std::cout << srcIp << ":" << srcPort << "|" << destIp << ":" << dstPort << "|" << protocol << std::endl;
+  sprintf(protocol, "%i", (int)tuple.protocol);
+  std::cout << srcIp << ":" << srcPort << "|" << destIp << ":" << dstPort << "|"
+            << protocol << std::endl;
 }
 using std::vector;
 typedef vector<FIVE_TUPLE> TRACE;
-#endif 
+#endif
--- src/fcmsketch.h
+++ src/fcmsketch.h
@@ -1,26 +1,40 @@
 #ifndef _FCM_SKETCH_H
 #define _FCM_SKETCH_H
 
+#include "BOBHash32.h"
 #include "common.h"
 #include "counter.h"
 #include "pds.h"
 #include <algorithm>
 #include <cstddef>
 #include <cstdint>
+#include <cstdio>
 #include <iostream>
 #include <ostream>
 #include <vector>
 
 class FCM_Sketch : public PDS {
+private:
   vector<Counter *> stages;
+  BOBHash32 hash;
+  uint32_t n_stages;
+  uint32_t *stages_sz;
+  uint32_t k;
+
+  int hashing(FIVE_TUPLE key) {
+    char c_ftuple[sizeof(FIVE_TUPLE)];
+    memcpy(c_ftuple, &key, sizeof(FIVE_TUPLE));
+    return hash.run(c_ftuple, 4) % stages_sz[0];
+  }
 
 public:
-  FCM_Sketch(uint32_t n_roots, uint32_t n_stages = 3, uint32_t k = 8) {
+  FCM_Sketch(uint32_t n_roots, uint32_t n, uint32_t n_stages = 3,
+             uint32_t k = 8) {
     // Check if structure is possible, max counter is 32bit
 
     // Maximum 32 bit counter
     uint32_t max_counter = 32;
-    uint32_t sz_stages[n_stages];
+    uint32_t *sz_stages = new uint32_t[n_stages];
     std::cout << "Got settings " << n_roots << " " << max_counter << std::endl;
     for (int i = n_stages - 1; i >= 0; --i) {
       sz_stages[i] = n_roots;
@@ -28,34 +42,57 @@ public:
       std::cout << sz_stages[i] << std::endl;
     }
 
+    // Setup stages with appropriate sizes
     for (size_t i = 0; i < n_stages; i++) {
       Counter *stage = new Counter[sz_stages[i]];
       for (size_t j = 0; j < sz_stages[j]; j++) {
         stage[j] = Counter(max_counter);
-        // std::cout << stage[j].count << std::endl;
       }
       stages.push_back(stage);
     }
+    this->hash.initialize(n);
+    this->stages_sz = sz_stages;
+    this->k = k;
+    this->n_stages = n_stages;
+  }
+
+  ~FCM_Sketch() {
+    for (auto v : this->stages) {
+      delete v;
+    }
+  }
+  int insert(FIVE_TUPLE tuple) {
+    int hash_idx = this->hashing(tuple);
     for (size_t s = 0; s < n_stages; s++) {
-      for (size_t i = 0; i < sz_stages[s]; i++) {
-        stages[s][i].increment();
-        std::cout << stages[s][i].count;
+      if (this->stages[s][hash_idx].overflow) {
+        // Check for complete overflow
+        if (s == n_stages - 1) {
+          return 1;
+        }
+        continue;
+      }
+      this->stages[s][hash_idx].increment();
+      char msg[30];
+      sprintf(msg, "(%i:%i) : %i\n", (int)s, hash_idx,
+              this->stages[s][hash_idx].count);
+      std::cout << msg;
+      break;
+    }
+    return 0;
+  }
+
+  void print_sketch() {
+    for (size_t s = 0; s < n_stages; s++) {
+      std::cout << "Stage " << s << " with size " << this->stages_sz[s]
+                << std::endl;
+      for (size_t i = 0; i < this->stages_sz[s]; i++) {
+        std::cout << this->stages[s][i].count << " ";
       }
       std::cout << std::endl;
     }
   }
-  //
-  // ~FCM_Sketch() {
-  //
-  //   for (auto v : this->stages) {
-  //     v.clear();
-  //   }
-  // }
-  int insert(FIVE_TUPLE tuple) { return 0; }
-  int remove(FIVE_TUPLE tuple) { return 0; }
-  int lookup(FIVE_TUPLE tuple) { return 0; }
-
-private:
+  // int remove(FIVE_TUPLE tuple) { return 0; }
+  // int lookup(FIVE_TUPLE tuple) { return 0; }
 };
 
 #endif // !_FCM_SKETCH_H

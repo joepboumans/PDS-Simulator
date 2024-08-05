810f8e36976c992b90e506d7ee288d71022ad43a
Joep Boumans <joep_boumans@live.nl>
2024-08-05 13:55:25 +0200

    Init CM Sketch; Counter vector initialization working

---
+++ src/count-min.cpp
@@ -0,0 +1,6 @@
+#ifndef _SIMULATOR_CPP
+#define _SIMULATOR_CPP
+
+#include "count-min.h"
+
+#endif // !_SIMULATOR_CPP
---
+++ src/count-min.h
@@ -0,0 +1,33 @@
+#ifndef _COUNT_MIN_H
+#define _COUNT_MIN_H
+
+// #include "BOBHash32.h"
+#include "common.h"
+#include "counter.h"
+#include "pds.h"
+#include <cstdint>
+#include <limits>
+#include <sys/types.h>
+#include <vector>
+
+class CountMin : public PDS {
+public:
+  vector<vector<Counter>> counters;
+  uint32_t row;
+  uint32_t columns;
+  uint32_t n_hash;
+  string trace_name;
+  CountMin(uint32_t row, uint32_t columns, uint32_t k, string trace)
+      : counters(row,
+                 vector<Counter>(
+                     columns, Counter(std::numeric_limits<uint32_t>::max()))) {
+    // Assign defaults
+    this->row = row;
+    this->columns = columns;
+    this->n_hash = k;
+    this->trace_name = trace;
+  }
+};
+
+#endif // !_COUNT_MIN_H
+#define _COUNT_MIN_H

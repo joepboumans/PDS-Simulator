2d0664594d52730503bf2860ea897dd91df26d31
Joep Boumans <joep_boumans@live.nl>
2024-08-08 12:10:36 +0200

    Add CM test

---
+++ test/cm-sketch.cpp
@@ -0,0 +1,12 @@
+#include "../src/count-min.h"
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Smoke Test", "[smoke-test]") {
+  REQUIRE(true == true);
+  REQUIRE(false == false);
+}
+
+TEST_CASE("Constructor FCM", "[const-CM]") {
+  CountMin count_min(4, 4, "test", 0, 0);
+  REQUIRE(typeid(count_min) == typeid(CountMin));
+}

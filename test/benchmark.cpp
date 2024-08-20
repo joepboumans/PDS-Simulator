88cb22b56e67e6cec8d889553871841e9262cbae
joepboumans <joep_boumans@live.nl>
2024-08-20 15:46:27 +0200

    Setup CHT benchmark

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -1,4 +1,5 @@
 #include "lib/count-min/count-min.h"
+#include "lib/cuckoo-hash/cuckoo-hash.h"
 #include "lib/simulator/simulator.h"
 #include "src/bloomfilter.h"
 #include "src/data-parser.h"
@@ -19,6 +20,22 @@ TEST_CASE("Benchmark BF", "[BF][Benchmark]") {
     bfilter.setName();
     bfilter.setupLogging();
     stage.push_back(&bfilter);
+    Simulator simulator(stage, stage.size(), 1);
+    simulator.run(trace, 60);
+  }
+}
+TEST_CASE("Benchmark CHT", "[CHT][Benchmark]") {
+  dataParser data_parser;
+  string data_set = "data/equinix-chicago-20160121-130000.dat";
+  TRACE trace;
+  data_parser.get_traces(data_set.data(), trace);
+
+  auto tables = GENERATE(3, 5, 8, 16, 32);
+  SECTION("Run CHT") {
+    auto sz = GENERATE(1024, 4 * 1024, 8 * 1024, 16 * 1024);
+    vector<PDS *> stage;
+    CuckooHash cuckoo(tables, sz, "benchmark", 0, 0);
+    stage.push_back(&cuckoo);
     Simulator simulator(stage, stage.size(), 1);
     simulator.run(trace, 60);
   }

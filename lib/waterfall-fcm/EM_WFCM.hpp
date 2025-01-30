8ad67a914c8ce336564cccff5115d519b604a00b
Joep Boumans <joep_boumans@live.nl>
2025-01-30 16:40:24 +0100

    Limit condi check on sketch_degree

--- lib/waterfall-fcm/EM_WFCM.hpp
+++ lib/waterfall-fcm/EM_WFCM.hpp
@@ -166,11 +166,11 @@ private:
       // Simplify higher sketch degree vc's
       if (in_sketch_degree > 1) {
         if (sum > 1100)
-          flow_num_limit = in_degree;
+          flow_num_limit = in_sketch_degree;
         else if (sum > 550)
-          flow_num_limit = std::max(in_degree, (uint32_t)3);
+          flow_num_limit = std::max(in_sketch_degree, (uint32_t)3);
         else
-          flow_num_limit = std::max(in_degree, (uint32_t)4);
+          flow_num_limit = std::max(in_sketch_degree, (uint32_t)4);
 
         // Note that the counters of degree D (> 1) includes at least D flows.
         // But, the combinatorial complexity also increases to O(N^D), which
@@ -191,10 +191,10 @@ private:
           flow_num_limit = 2;
           in_degree = 2;
         }
-        printf("Setup gen with sum:%d, in_degree:%d, in_sketch_degree:%d, "
-               "flow_num_limit:%d\n",
-               sum, in_degree, in_sketch_degree, flow_num_limit);
-        print_thresholds();
+        /*printf("Setup gen with sum:%d, in_degree:%d, in_sketch_degree:%d, "*/
+        /*       "flow_num_limit:%d\n",*/
+        /*       sum, in_degree, in_sketch_degree, flow_num_limit);*/
+        /*print_thresholds();*/
       } else if (sum > 100) {
         max_small = 2;
       }
@@ -218,10 +218,12 @@ private:
         int remain = sum - partial_sum;
         if (remain >= now_result[now_flow_num - 2]) {
           now_result[now_flow_num - 1] = remain;
+          /*print_now_result();*/
           return true;
         }
       }
 
+      /*print_now_result();*/
       return false;
     }
 
@@ -248,10 +250,10 @@ private:
               return true;
             }
             if (check_condition()) {
-              if (in_degree > 2) {
-                print_now_result();
-                exit(1);
-              }
+              /*if (in_degree > 2) {*/
+              /*  print_now_result();*/
+              /*  exit(1);*/
+              /*}*/
               total_combi++;
               return true;
             }
@@ -309,15 +311,12 @@ private:
       }
 
       for (auto &t : thresh) {
-        uint32_t colls = t[2];
+        uint32_t colls = t[1];
 
         if (colls <= 1) {
           continue;
         }
 
-        if (&t == &thresh.front()) {
-          continue;
-        }
         /*print_thresholds();*/
         /*print_now_result();*/
 
